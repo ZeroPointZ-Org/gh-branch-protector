91b3a4591d0d7e4d9bd49559ffcbb99bd6dc2a87
Mike D <mikedorado@gmail.com>
2021-12-15 00:52:06 -0800

    quick fix

--- app.py
+++ app.py
@@ -47,8 +47,7 @@ def webhook():
                             "title": "New Protection Added",
                             "body": "Dont Panic @"
                             + user
-                            + "! A new branch protection was added to the default branch."
-                            + branch_protection,
+                            + "! A new branch protection was added to the default branch.",
                         }
                         session = requests.session()
                         session.auth = (user, cred)
